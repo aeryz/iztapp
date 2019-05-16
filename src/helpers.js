a88cafe4524502cba74d01381bd0dbcb2031c686
Bora Baloglu <baloglub@gmail.com>
2019-05-16 20:54:27 +0300

    feat(src/helpers.js): Validate and hash helper functions are added

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,53 @@
-/*
+import config from "./config";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
 
-  Helper functions will be located in this file.
+async function validate(data, type, noError = false) {
+	try {
+		switch (type) {
+			case "id": {
+				data += "";
+				const lengthOfData = data.length;
+				let isValid = false;
+				if (lengthOfData === 12 || lengthOfData === 24) isValid = (/^[0-9a-fA-F]+$/).test(data);
+				if (!isValid) throw new Error(config.errors.INVALID_ID);
+				break;
+			}
+			case "email": {
+				if (!validator.isEmail(data)) throw new Error(config.errors.INVALID_EMAIL);
+				break;
+			}
+			case "supportedLang": {
+				if (!config.supportedLangs.includes(data)) throw new Error(config.errors.UNSUPPORTED_LANG);
+				break;
+			}
+			case "isEmail": {
+				if (validator.isEmail(data)) return true;
+				return false;
+			}
+			default: {
+				throw new Error("Invalid validation type");
+			}
+		}
+		return true;
+	} catch (err) {
+		if (noError === true) return false;
+		throw err;
+	};
+};
 
-  e.g -> generateHash, generatePasswordHash, generateUrlString, comparePassword etc.
+async function generateHash(data) {
+	return crypto.createHash(config.hashAlgorithm).update(data).digest("hex");
+}
 
-*/
+async function generatePasswordHash(password, salt) {
+	return bcrypt.hash(password, salt);
+}
+
+
+export default {
+	validate,
+	generateHash,
+	generatePasswordHash,
+};
