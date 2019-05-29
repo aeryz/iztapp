0f11a3af06a4a947f5f17d52c50992b77afa686e
abdullah-eryuzlu <abdullaheryuzlu@gmail.com>
2019-05-29 09:05:27 -0700

    feat(config.js, router.js): config and router edited

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,6 +4,7 @@ import AccountController from '../controllers/accountController.js';
 
 var router = Router();
 
+// ACCOUNT CONTROLLER >>
 
 router.get('/api/get/accounts/:limit/:skip/:accountType',
   async(ctx) => {
@@ -82,5 +83,302 @@ router.post('/api/unlock/account/:hash',
   async(ctx) => {
     helpers.authenticateAdmin(ctx);
     ctx.body = await AccountController.delete(ctx.params.id);
+  }
+)
+
+router.post(' /api/update/password/:id',
+  async(ctx) => {
+    helpers.authenticateAdmin(ctx);
+    ctx.body = await AccountController.updatePassword(ctx.params.id, ctx.body.newPassword);
+  }
+)
+
+// << ACCOUNT CONTROLLER
+
+// WORKER CONTROLLER >>
+
+router.post('/api/get/workers',
+  async(ctx) => {
+    if (!helpers.validate(ctx.body.limit, "paramNumber")
+     || !helpers.validate(ctx.body.skip, "paramNumber")) {
+       throw new Error(config.errors.PARAMETER_ERROR);
+     }
+
+    ctx.body = await WorkerController.getWorkers(
+      ctx.body.limit,
+      ctx.body.skip,
+      ctx.body.query
+    )
+  }
+)
+
+// << WORKER CONTROLLER
+
+// EMAIL CONTROLLER >>
+
+router.get('/api/get/emails/:limit/:skip',
+  async(ctx) => {
+    if (!helpers.validate(ctx.param.limit, "paramNumber")
+     || !helpers.validate(ctx.param.skip, "paramNumber")) {
+       throw new Error(config.errors.PARAMETER_ERROR);
+     }
+
+    ctx.body = await EmailController.getEmails(
+      ctx.body.limit,
+      ctx.body.skip
+    )
+  }
+)
+
+router.post('/api/add/email',
+  async(ctx) => {
+
+    ctx.body = await EmailController.add(
+      ctx.body
+    )
+  }
+)
+
+router.get('/api/delete/email/:id',
+  async(ctx) => {
+
+    ctx.body = await EmailController.delete(
+      ctx.params.id
+    )
+  }
+)
+
+// << EMAIL CONTROLLER
+
+// EMAIL LIST CONTROLLER >>
+router.get('/api/get/emailLists/:limit/:skip',
+  async(ctx) => {
+    if (!helpers.validate(ctx.param.limit, "paramNumber")
+     || !helpers.validate(ctx.param.skip, "paramNumber")) {
+       throw new Error(config.errors.PARAMETER_ERROR);
+     }
+
+    ctx.body = await EmailListController.getEmailLists(
+      ctx.params.limit,
+      ctx.params.skip
+    )
+  }
+)
+
+router.get('/api/get/emailLists/:id',
+  async(ctx) => {
+
+    ctx.body = await EmailListController.geetEmailListById(
+      ctx.params.id,
+    )
+  }
+)
+
+router.post('/api/add/emailList',
+  async(ctx) => {
+
+    ctx.body = await EmailListController.add(
+      ctx.body
+    )
+  }
+)
+
+router.post('/api/update/emailList',
+  async(ctx) => {
+
+    ctx.body = await EmailListController.update(
+      ctx.body
+    )
+  }
+)
+
+router.get('/api/delete/emailList/:id',
+  async(ctx) => {
+
+    ctx.body = await EmailListController.delete(
+      ctx.body
+    )
+  }
+)
+
+router.post('/api/append/emailList',
+  async(ctx) => {
+
+    ctx.body = await EmailListController.addEmailToList(
+      ctx.body.emailId,
+      ctx.body.emailListId
+    )
+  }
+)
+
+router.post('/api/remove/email',
+  async(ctx) => {
+
+    ctx.body = await EmailListController.deleteEmailFromList(
+      ctx.body.emailId,
+      ctx.body.emailListId
+    )
+  }
+)
+
+// << EMAILLISTCONTROLLER
+
+// EVENTCONTROLLER >>
+router.post('/api/pull/events',
+  async(ctx) => {
+
+    ctx.body = await EventController.pullEvents();
+  }
+)
+
+router.post('/api/send/events',
+  async(ctx) => {
+
+    if (ctx.body.event === null || ctx.body.event === "") {
+      throw new Error(config.errors.UNFILLED_REQUIREMENTS);
+    }
+
+    ctx.body = await EventController.sendEvent(
+      ctx.body.event,
+      ctx.body.emailListId
+    )
+  }
+)
+
+// << EVENTCONTROLLER
+
+// SCHEDULECONTROLLER >>
+
+router.post('/api/get/weekly/:limit/:skip',
+  async(ctx) => {
+    if (!helpers.validate(ctx.param.limit, "paramNumber")
+     || !helpers.validate(ctx.param.skip, "paramNumber")) {
+       throw new Error(config.errors.PARAMETER_ERROR);
+     }
+
+    ctx.body = await ScheduleController.getWeeklySchedules(
+      ctx.params.limit,
+      ctx.params.skip,
+      ctx.body
+    )
+  }
+)
+
+router.post('/api/get/weekly',
+  async(ctx) => {
+
+    ctx.body = await ScheduleController.getWeeklySchedule(
+      ctx.body
+    )
+  }
+)
+
+router.post('/api/get/daily/:limit/:skip',
+  async(ctx) => {
+    if (!helpers.validate(ctx.param.limit, "paramNumber")
+     || !helpers.validate(ctx.param.skip, "paramNumber")) {
+       throw new Error(config.errors.PARAMETER_ERROR);
+     }
+
+    ctx.body = await ScheduleController.getDailySchedules(
+      ctx.params.limit,
+      ctx.params.skip,
+      ctx.body
+    )
+  }
+)
+
+router.post('/api/get/daily',
+  async(ctx) => {
+
+    ctx.body = await ScheduleController.getDailySchedule(
+      ctx.body
+    )
+  }
+)
+
+router.post('/api/add/weeklySchedule',
+  async(ctx) => {
+
+    ctx.body = await ScheduleController.addWeeklySchedule(
+      ctx.body
+    )
+  }
+)
+
+router.post('/api/update/weeklySchedule/:id',
+  async(ctx) => {
+
+    ctx.body = await ScheduleController.updateWeeklySchedule(
+      ctx.params.id,
+      ctx.body
+    )
+  }
+)
+
+router.get('/api/delete/weeklySchedule/:id',
+  async(ctx) => {
+
+    ctx.body = await ScheduleController.deleteWeeklySchedule(
+      ctx.params.id
+    )
+  }
+)
+
+router.post('/api/add/dailySchedule',
+  async(ctx) => {
+
+    ctx.body = await ScheduleController.addDailySchedule(
+      ctx.body
+    )
+  }
+)
+
+router.post('/api/update/dailySchedule/:id',
+  async(ctx) => {
+
+    ctx.body = await ScheduleController.updateDailySchedule(
+      ctx.params.id,
+      ctx.body
+    )
+  }
+)
+
+router.post('/api/delete/dailySchedule/:id',
+  async(ctx) => {
+
+    ctx.body = await ScheduleController.deleteDailySchedule(
+      ctx.params.id
+    )
+  }
+)
+
+router.post('/api/assign/dailyToWeekly/:dailyScheduleId/:weeklyScheduleId/:day',
+  async(ctx) => {
+
+    if (Number.isNaN(ctx.params.day) || ctx.params.day < 0 || ctx.params.day > 4) {
+      throw new Error(config.errors.PARAMETER_ERROR);
+    }
+
+    ctx.body = await ScheduleController.assignDailyToWeekly(
+      ctx.params.weeklyScheduleId,
+      ctx.params.dailyScheduleId,
+      ctx.params.day
+    )
+  }
+)
+
+router.post('/api/remove/dailyFromWeekly/:dailyScheduleId/:weeklyScheduleId/:day',
+  async(ctx) => {
+
+    if (Number.isNaN(ctx.params.day) || ctx.params.day < 0 || ctx.params.day > 4) {
+      throw new Error(config.errors.PARAMETER_ERROR);
+    }
+
+    ctx.body = await ScheduleController.assignDailyToWeekly(
+      ctx.params.weeklyScheduleId,
+      ctx.params.dailyScheduleId,
+      ctx.params.day
+    )
   }
 )
